518f7ee9b63fa9bddb95fe54ef8518163b13d199
Abegail Lasco <ivonnelasco01@gmail.com>
2022-11-05 20:52:16 +0800

    description

--- seatwork 1.py
+++ seatwork 1.py
@@ -2,10 +2,17 @@
 list1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 #menu
-#add an element
-print("----------------------")
-print("Array:", list1)
-print("Menu:")
-print("1. Add an element")
-print("----------------------")
+def menu():
+    print("----------------------")
+    print("Array:", list1)
+    print("Menu:")
+    print("1. Add an element")
+    print("2. Insert an element")
+    print("3. Modify an element")
+    print("4. Delete an element")
+    print("5. Arrange in descending order")
+    print("6. Arrange in accending order")
+    print("----------------------")
+menu()
+
 
