747ce8e3735f3e793e5afb85957cd95fcedff6b7
Abegail Lasco <ivonnelasco01@gmail.com>
2022-11-05 20:50:05 +0800

    description

--- seatwork 1.py
+++ seatwork 1.py
@@ -1,3 +1,11 @@
 #list 
 list1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-print(list1)
+
+#menu
+#add an element
+print("----------------------")
+print("Array:", list1)
+print("Menu:")
+print("1. Add an element")
+print("----------------------")
+
