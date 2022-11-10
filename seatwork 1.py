ca88cedc605b420b877c45758f2b28266222f90a
ivonne.lasco <92631714+Ivonnelasco01@users.noreply.github.com>
2022-11-11 01:32:04 +0800

    Update seatwork 1.py

--- seatwork 1.py
+++ seatwork 1.py
@@ -1,48 +1,48 @@
-list = [2, 5, 6, 8, 9, 11, 15, 1, 85, 95, 10]
-print("------------------------")
-print("Array:", list)
-print("")
-print("Menu:")
-print("1 -> Add an element")
-print("2 -> Insert an element")
-print("3 -> Modify an element")
-print("4 -> Delete an elementt")
-print("5 -> Arrange in ascending order")
-print("6 -> Arrange in descending order")
-print("-----------------------")
-print()
-op = int(input("What do you want to do? Pick a number from 1-6 >>"))
-def num1():
-    if op == 1:
-        insert_num = int(input("What number do you want to add? "))
-        result = list.append(insert_num)
-        print("Your number has been added")
-        return result
-    elif op == 2:
+list=[10, 20, 45, 6, 78, 11, 5, 12, 18, 15]
+def menu():
+    print("This is the array", list)
+    print("MENU:")
+    print("1. Add an element")
+    print("2. Insert an element")
+    print("3. Modify an element")
+    print("4. Delete an element")
+    print("5. Arrange in ascending order")
+    print("6. Arrange in descending order")
+menu()
+
+while True:
+    select = int(input("Select a number >> "))
+    if select == 1:
+        add_num = int(input("What number do you want to add? "))
+        result = list.append(add_num)
+        print("Your number has been added, new array :", list)
+        break
+    elif select == 2:
         insert_num2 = int(input("What number do you want to insert?"))
         insert_num3 = int(input("Where do you want to insert?"))
-        result_2 = list.insert(insert_num3, insert_num2)
-        print("The element has been Insert")
-        return result_2    
-    elif op == 3:
-        insert_num4 = int(input("What number do yoy want to modify?"))
+        result_2 = list.insert(insert_num3 - 1, insert_num2)
+        print("Your number has been insert, new array :", list)
+        break
+    elif select == 3:
+        insert_num4 = int(input("From the arrangement 1-10, modify a number:"))
         insert_num5 = int(input("Insert your number:"))
-        result_3 = list.pop(insert_num4)
-        list.insert(insert_num4, insert_num5)
-        print("The element has been Modify")
-        return result_3
-    elif op == 4:
+        result_3 = list.pop(insert_num4 -1)
+        list.insert(insert_num4 -1, insert_num5)
+        print("Your number has been modified, new array :", list)
+        break
+    elif select == 4:
         insert_num6 = int(input("What number do you want to delete?"))
         result_4 = list.remove(insert_num6)
-        return result_4
-    elif op == 5:
+        print("Your number has been deleted, new array :", list)
+        break
+    elif select == 5:
         result_5 = list.sort()
-        print("The element has been arrange to ascending order")
-        return result_5
-    elif op == 6:
-        result_6 = list.reverse()
-        print("The element has been arrange to descending order")
-        return result_6
-num1()
-print()
-print(f"This is the new array: Array : {list}")
+        print("Arrangement in ascending order, new array :", list)
+        break
+    elif select == 6:
+        result_6 = list.sort(reverse=True)
+        print("Arrangement in descending order, new array ", list)
+        break
+    else:
+        print("THATS NOT IN THE MENU, PICK ANOTHER ONE!")
+        menu()
